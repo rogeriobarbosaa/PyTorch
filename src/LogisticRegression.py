693bdc34a98bcf7e13a2a18e915bf9e627de62de
rogeriobarbosa07 <rogeriodosanjosbarbosa2004@gmail.com>
2026-07-25 01:22:41 -0300

    feat: adiciona super da logistic regression. fix: conserta declaração da init

--- src/LogisticRegression.py
+++ src/LogisticRegression.py
@@ -2,7 +2,8 @@ import torch
 from torch import nn
 
 class LogisticRegression(nn.Module):
-    def __init_(self, n_input):
+    def __init__(self, n_input):
+        super(LogisticRegression, self).__init__()
         self.linear = nn.Linear(n_input, 1)
         self.act = nn.Sigmoid()
 
